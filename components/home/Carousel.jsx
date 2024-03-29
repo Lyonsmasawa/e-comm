4bf5f4d2f9a65c08ca3746f8fd1a7723f4898e5c
Lyonsmasawa <lyonsmasawa@gmail.com>
2024-03-29 04:18:37 -0700

    carouself

--- components/home/Carousel.jsx
+++ components/home/Carousel.jsx
@@ -1,20 +1,33 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { SliderBox } from "react-native-image-slider-box";
+import { COLORS } from "../../constants/theme";
 
 const Carousel = () => {
   const slides = [
-    "https://images.unsplash.com/photo-1600121848594-d8644e57abab?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDZ8fGZ1cm5pdHVyZXxlbnwwfHwwfHx8MA%3D%3D",
-    "https://images.unsplash.com/photo-1616486338812-3dadae4b4ace?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGZ1cm5pdHVyZXxlbnwwfHwwfHx8MA%3D%3D",
     "https://plus.unsplash.com/premium_photo-1673548917423-073963e7afc9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZ1cm5pdHVyZXxlbnwwfHwwfHx8MA%3D%3D",
+    "https://plus.unsplash.com/premium_photo-1681046751108-a516bea00570?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8",
+    "https://plus.unsplash.com/premium_photo-1676823570926-238f23020786?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   ];
   return (
-    <View>
-      <Text>Carousel</Text>
+    <View style={styles.carouselContainer}>
+      <SliderBox
+        images={slides}
+        dotColor={COLORS.primary}
+        inactiveDotColor={COLORS.secondary}
+        ImageComponentStyle={{ borderRadius: 15, width: "93%", marginTop: 15 }}
+        autoplay
+        circleLoop
+      />
     </View>
   );
 };
 
 export default Carousel;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  carouselContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
