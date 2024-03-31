39affae3a917e54601ff7cd529305df6386aea70
Lyonsmasawa <lyonsmasawa@gmail.com>
2024-03-31 00:34:49 -0700

    hook

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,12 +1,36 @@
 import { View, Text } from "react-native";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 const useFetch = () => {
-  return (
-    <View>
-      <Text>useFetch</Text>
-    </View>
-  );
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.get("localhost:3000/api/products/");
+      setData(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
 };
 
 export default useFetch;
